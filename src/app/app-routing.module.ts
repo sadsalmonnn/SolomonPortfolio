5b71969056d7ac672a8375f0ffab4c2e283fd0ef
dionis17 <solomon.adordionisio@mail.utoronto.ca>
2025-07-03 16:41:42 -0400

    changed assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component'; // adjust path as needed
+// import other components...
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent }, // 👈 Home is now the root
+  // optionally: wildcard route for 404
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
